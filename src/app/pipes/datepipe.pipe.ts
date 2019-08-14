267117aafbd954a22e711f9c4bc82e50df7d642d
cha3 <st.chop2skk@gmail.com>
2019-08-13 21:56:14 -0700

    fix datepipe

--- src/app/pipes/datepipe.pipe.ts
+++ src/app/pipes/datepipe.pipe.ts
@@ -9,8 +9,26 @@ export class DatepipePipe extends DatePipe implements PipeTransform {
   
 datePipe = new DatePipe('en-US');
 
-transform(unixTime: number): string {
-  const datetime = new Date(unixTime);
-    return this.datePipe.transform(datetime, 'HH:mm');
+  transform(unixTime: number): string {
+    const dateUnixTime = new Date(unixTime)
+    const datetime = this.changeFormat(new Date(unixTime));
+    const now = this.changeFormat(new Date());
+    
+    let format = "";
+    if(datetime.year === now.year && datetime.month === now.month && datetime.date === now.date){
+      format = this.datePipe.transform(dateUnixTime, 'HH:mm'); 
+    } else {
+      format = this.datePipe.transform(dateUnixTime, 'MM/dd');
+    }
+    return format;
+  }
+
+  changeFormat(datetime){
+    const targetDatetime = {
+      year:datetime.getFullYear(),
+      month:datetime.getMonth() + 1,
+      date:datetime.getDate()
+    };
+    return targetDatetime;
   }
 }
